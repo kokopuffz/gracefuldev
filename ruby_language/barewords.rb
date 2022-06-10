bb87f8fbee0e5720ff9a6939395c06eb1893c413
grace <chanheeya@gmail.com>
2022-06-10 02:31:30 -0500

    put into method class

--- ruby_language/barewords.rb
+++ ruby_language/barewords.rb
@@ -1,12 +1,16 @@
-salutation = "Most agreeable to see you"
-title = "Commander"
-full_name = "Sam Vimes"
-progname  = "Dis-organizer"
-version = "Mark 7"
-designation = " Seeree"
-service_inquiry = "order you a coffee"
+class ObsequiousImp
+  def greet(title, full_name)
+    salutation = "Most agreeable to see you"
+    progname  = "Dis-organizer"
+    version = "Mark 7"
+    designation = " Seeree"
+    service_inquiry = "order you a coffee"
 
-puts "#{salutation}, #{title} #{full_name}. ",
-     "Welcome to #{progname} version #{version}. ",
-     "My name is #{designation}. ",
-     "May I #{service_inquiry}"
+    puts "#{salutation}, #{title} #{full_name}. ",
+         "Welcome to #{progname} version #{version}. ",
+         "My name is #{designation}. ",
+         "May I #{service_inquiry}"
+  end
+end
+ObsequiousImp.new.greet("Commander", "Sam Vimes")
+
